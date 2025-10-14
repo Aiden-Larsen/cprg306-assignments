5544655c50ecfb3ec939b312d119fcc659c16f3b
Aiden <aiden.larsen@edu.sait.ca>
2025-10-13 22:42:56 -0600

    week-5 assignment complete

---
+++ app/week-5/new-item.js
@@ -0,0 +1,149 @@
+"use client";
+
+import { useState } from "react";
+
+export default function NewItem() {
+    const [quantity, setQuantity] = useState(1);    // holds amount in quantity
+    const [name, setName] = useState("");   // holds name
+    const [category, setCategory] = useState("Produce");
+    const [inDisabled, setInDisabled] = useState(false);    // holds state of buttons
+    const [deDisabled, setDeDisabled] = useState(true);
+
+    // qunatity + 1
+    const increment = () => {
+        setQuantity(quantity + 1);
+        buttonSwitch(1);       
+    }
+
+    // quantity - 1
+    const decrement = () => {
+        setQuantity(quantity - 1);
+        buttonSwitch(2);
+    }
+
+    // buttonSwitch works to enable/disable both button depending on the amount in quantity
+    const buttonSwitch = (button) => {
+        if (button == 1) {  // 1 = increment
+            if (quantity+1 >= 20) {
+                setInDisabled(true);
+            } else if (quantity+1 < 20) {
+                setInDisabled(false);   // if quantity is incremented than re-enable decrement
+                setDeDisabled(false);
+            }
+        } else if (button == 2) {   // 2 = decrement
+            if (quantity-1 <= 1) {
+                setDeDisabled(true);
+            } else if (quantity-1 > 1) {
+                setDeDisabled(false);   // if quantity is decremented than re-enable increment
+                setInDisabled(false);
+            }
+        }
+    }
+
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+    }
+
+    const handleCategory = (event) => {
+        setCategory(event.target.value);
+        console.log();
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        /*
+        * creates object of the item's values *
+        */
+        let item = {
+            name: name,
+            quantity: quantity,
+            category: category,
+        };
+        // logs the individual values to the console
+        console.log(`${item.name}, ${item.quantity}, ${item.category}`);
+        // sends alert to user listing values
+        alert(
+            `Item: ${item.name}
+Quantity: ${item.quantity}
+Category: ${item.category}`
+        );
+        // resets values to initial
+        setName(""), setQuantity(1), setCategory("produce");
+    }
+
+    return (
+        <form onSubmit={handleSubmit} className="rounded bg-blue-400 gap-3 mx-2.5 p-2">
+            <section>
+                <section className="rounded bg-blue-300 p-2 mb-4">
+                    <div>
+                        <label className="block font-bold text-gray-600 p-1">Item Name</label>
+                        <input 
+                        type="text" 
+                        placeholder="e.g. bread: 1, Bakery" 
+                        required 
+                        value={name} 
+                        onChange={handleNameChange} 
+                        className="rounded border border-gray-600 text-gray-500 bg-blue-200 p-2 mx-2 my-1"
+                        ></input>
+                    </div>
+                </section>
+                <section className="rounded bg-blue-300 p-2 mb-4">
+                    <div className="p-1 mb-2">
+                        <p className="text-gray-600 font-bold">Quantity (1-20)</p>
+                    </div>
+                    <section className="flex mx-2 gap-3">
+                        <div>
+                            <button
+                            className="rounded text-m font-bold px-4 py-2 bg-blue-200 text-gray-600 active:bg-blue-50 disabled:bg-gray-400"
+                            type="button"
+                            onClick={increment}
+                            disabled={inDisabled}
+                            > + </button>
+                        </div>
+                        <div>
+                            <button
+                                className="rounded text-m font-bold px-4 py-2 bg-blue-200 text-gray-600 active:bg-blue-50 disabled:bg-gray-400"
+                                type="button"
+                                onClick={decrement}
+                                disabled={deDisabled}
+                            > - </button>
+                        </div>
+                    </section>
+                    <section>
+                        <div className="flex rounded bg-blue-300 px-2 py-3">
+                            <p className="rounded text-gray-600 text-xl font-bold bg-blue-200 px-4 py-2">Current: {quantity}</p>
+                        </div>
+                    </section>
+                </section>
+                <section className="rounded bg-blue-300 p-2 mb-4">
+                        <div>
+                            <label htmlFor="category" className="block text-gray-600 font-bold p-1">Category</label>
+                            <select 
+                            id="category"
+                            value={category}
+                            onChange={handleCategory} 
+                            className="rounded border border-gray-600 text-gray-500 bg-blue-200 p-2 mx-2 my-1">
+                                <option value="Produce" >Produce</option>
+                                <option value="Dairy">Dairy</option>
+                                <option value="Bakery">Bakery</option>
+                                <option value="Meat">Meat</option>
+                                <option value="Frozen Foods">Frozen Foods</option>
+                                <option value="Canned Goods">Canned Goods</option>
+                                <option value="Dry Goods">Dry Goods</option>
+                                <option value="Beverages">Beverages</option>
+                                <option value="Snacks">Snacks</option>
+                                <option value="Household">Household</option>
+                                <option value="Other">Other</option>
+                            </select>
+                        </div>
+                </section>
+                <section className="rounded bg-blue-300 p-2">
+                    <button 
+                    type="submit"
+                    className="rounded text-m font-bold px-4 py-2 bg-blue-200 text-gray-600 active:bg-blue-50"
+                    >Add Item</button>
+                </section>
+            </section>
+        </form>
+    )
+}
---
+++ app/week-5/page.js
@@ -0,0 +1,15 @@
+import NewItem from "./new-item";
+
+export default function Page() {
+    return (
+        <main className="rounded maw-w-md mx-auto pb-4 bg-blue-800">
+            <header className="rounded bg-blue-500 pb-3">
+                <h1 className="text-3xl font-bold text-gray-900 pl-2">Week-5</h1>
+                <p className="text-gray-900 font-bold pl-2">:)</p>
+            </header>
+            <section className="rounded gap-4 mt-4 mx-10 py-4 bg-blue-500">
+                <NewItem/>
+            </section>
+        </main>
+    ) 
+}
